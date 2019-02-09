85971b6112c6b1c2eacb56d9001f95751738b432
tudor96 <gltudor96@gmail.com>
2019-02-09 15:55:40 +0200

    Updates

---
+++ precache-manifest.a91778aaff7e39d7392c0a041d12d6e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a7187273b38115be64e1",
+    "url": "/face_recognition/static/js/runtime~main.a7187273.js"
+  },
+  {
+    "revision": "c0606eb7fbb7e590d143",
+    "url": "/face_recognition/static/js/main.c0606eb7.chunk.js"
+  },
+  {
+    "revision": "213f45d4348c75fb7472",
+    "url": "/face_recognition/static/js/1.213f45d4.chunk.js"
+  },
+  {
+    "revision": "c0606eb7fbb7e590d143",
+    "url": "/face_recognition/static/css/main.59b1eff1.chunk.css"
+  },
+  {
+    "revision": "213f45d4348c75fb7472",
+    "url": "/face_recognition/static/css/1.abab741a.chunk.css"
+  },
+  {
+    "revision": "976cac6352fef4b6c24968091995f047",
+    "url": "/face_recognition/index.html"
+  }
+];
